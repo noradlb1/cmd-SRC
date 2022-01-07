4002d36c74732a32e4804a251650744cf2f1f890
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2022-01-07 05:12:07 +0100

    cmd-SRC
    cmd-SRC

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.BackColor = System.Drawing.Color.Black
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.White
+        Me.RichTextBox1.Location = New System.Drawing.Point(3, 6)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(568, 233)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboBox1.BackColor = System.Drawing.Color.Black
+        Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ComboBox1.ForeColor = System.Drawing.Color.White
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(3, 240)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(568, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(572, 262)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "CMD"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,47 @@
+﻿'CODE BY TALENT1
+Public Class Form1
+    Dim cdd() As String
+    Private Sub ComboBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles ComboBox1.KeyDown
+        If e.KeyData = Keys.Enter Then
+
+            Dim process As Process = New Process
+            With process
+                .StartInfo.ErrorDialog = False
+                .StartInfo.RedirectStandardInput = True
+                .StartInfo.RedirectStandardOutput = True
+                .StartInfo.UseShellExecute = False
+                .StartInfo.FileName = "cmd.exe"
+                .StartInfo.CreateNoWindow = True
+                .StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+
+            End With
+            process.Start()
+            Try
+                process.StandardInput.WriteLine("cd/d " & cdd(3))
+            Catch
+            End Try
+            process.StandardInput.WriteLine(ComboBox1.Text)
+            process.StandardInput.WriteLine("echo [~cd~]%cd%[~cd~]")
+            process.StandardInput.WriteLine("exit")
+            Dim outpp As New TextBox
+            outpp.Text = process.StandardOutput.ReadToEnd
+            cdd = Split(outpp.Text, "[~cd~]")
+
+            For i = 4 To outpp.Lines.Length - 6
+                RichTextBox1.Text = RichTextBox1.Text & outpp.Lines(i).Substring(0) & vbCrLf
+            Next
+            ComboBox1.Items.Add(ComboBox1.Text)
+            ComboBox1.Text = ""
+            RichTextBox1.SelectionStart = RichTextBox1.Text.Length
+            RichTextBox1.ScrollToCaret()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        RichTextBox1.Text = "Microsoft Windows [Version " & My.Computer.Info.OSVersion & "]"
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+End Class
